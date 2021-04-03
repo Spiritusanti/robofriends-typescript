d239960bc326b863c27f528430845bfab35c87be
Spiritusanti <mccrackenjacob@gmail.com>
2021-04-03 10:23:52 -0700

    added in types to all components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 
-const Card = ({ name, email, id }) => {
+interface CardProps {
+  name: string;
+  email: string;
+  id: number;
+}
+
+const Card: React.FC<CardProps> = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
